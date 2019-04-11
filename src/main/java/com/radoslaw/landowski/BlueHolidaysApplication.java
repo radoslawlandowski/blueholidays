591785577644d044f7e5bff60609c0c112afe659
Radosław Landowski <radoslaw.landowski@gmail.com>
2019-04-11 18:38:10 +0200

    Add postman collections, setup aws serverless deployment

--- src/main/java/com/radoslaw/landowski/BlueHolidaysApplication.java
+++ src/main/java/com/radoslaw/landowski/BlueHolidaysApplication.java
@@ -4,10 +4,11 @@ import com.radoslaw.landowski.config.BlueHolidaysConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(BlueHolidaysConfig.class)
-public class BlueHolidaysApplication {
+public class BlueHolidaysApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlueHolidaysApplication.class, args);
