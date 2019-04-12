722f3253bb25412eaefbfa1c79259d3749349801
Radosław Landowski <radoslaw.landowski@gmail.com>
2019-04-12 09:18:02 +0200

    Make a nested class for holidayapipl

--- src/main/java/com/radoslaw/landowski/service/obtainers/holidayapipl/model/HolidayApiPlResponse.java
+++ src/main/java/com/radoslaw/landowski/service/obtainers/holidayapipl/model/HolidayApiPlResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @NoArgsConstructor
@@ -12,4 +14,43 @@ import java.util.List;
 public class HolidayApiPlResponse {
     public List<Holiday> holidays;
     public int status;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Holiday {
+        public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+        private String name;
+        private String country;
+        private String date;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public LocalDate getDate() {
+            return parseDate(date);
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        private LocalDate parseDate(String date) {
+            return LocalDate.parse(date, DateTimeFormatter.ofPattern(DATE_FORMAT));
+        }
+    }
+
 }
